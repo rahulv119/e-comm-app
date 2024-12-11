869167400dc9e631abb5e9aead567807eec0bd5c
unknown <rahul.angarai@gmail.com>
2024-12-11 21:16:28 +0530

    user_model

---
+++ lib/models/user_model.dart
@@ -0,0 +1,19 @@
+class User {
+  final String username;
+  final String password;
+  final String firstname;
+  final String lastname;
+  final String address;
+  final String email;
+  final String phone;
+
+  const User({
+    required this.username,
+    required this.password,
+    required this.firstname,
+    required this.lastname,
+    required this.address,
+    required this.email,
+    required this.phone,
+  });
+}
